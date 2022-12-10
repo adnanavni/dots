f06753bc6e6357f37cac0a9627c19a048b505041
Adnan <adnan.avni@hotmail.com>
2022-12-10 23:35:34 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+  const [points, setPoints] = useState([])
+  const [popped, setPopped] = useState([])
+
+  const handleCircle = (e) => {
+    const { clientX, clientY } = e.nativeEvent
+    setPoints([...points, { x: clientX, y: clientY }])
+  }
+
+  const handleUndo = () => {
+    const newPoints = [...points]
+    const poppedPoint = newPoints.pop()
+    if (!poppedPoint) return
+    setPopped([...popped, poppedPoint])
+    setPoints(newPoints)
+  }
+
+  const handleRedo = () => {
+    const newPopped = [...popped]
+    const poppedPoint = newPopped.pop()
+    if (!poppedPoint) return
+    setPoints([...points, poppedPoint])
+    setPopped(newPopped)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2>Press anywhere in the blue area</h2>
+      <button onClick={handleUndo} disabled={points.length === 0}>undo</button>
+      <button onClick={handleRedo} disabled={popped.length === 0}>redo</button>
+      <div className="App" onClick={handleCircle}>
+        {points.map(point => (
+          <div key={point.x} className='point' style={{
+            left: point.x - 5 + 'px',
+            top: point.y - 5 + 'px'
+          }}></div>
+        ))}
+      </div>
+    </>
   );
 }
 
